ce1de930ca78a8eee94676e696a0bc34868eece8
gosiagrajewska <149269502+gosiagrajewska@users.noreply.github.com>
2024-06-17 17:02:06 +0200

    links added in footer and styles.css removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "./App.css";
 import Search from "./Search";
 
 export default function App() {
@@ -7,6 +7,33 @@ export default function App() {
       <h1>Weather Search Engine</h1>
       <Search />
       <h2> </h2>
+
+      <p>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/gosiagrajewska"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Malgorzata Grajewska
+        </a>{" "}
+        and is open sourced on{" "}
+        <a
+          href="https://github.com/gosiagrajewska/meteo-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://meteoreactgosia.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </p>
     </div>
   );
 }
